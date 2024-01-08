dfbb5bf161b9056da31ec70b689d569e1e529c25
Dmitriy Lopatiev <dmitriy.lopatiev@gmail.com>
2024-01-08 23:07:44 +0200

    added console output

--- AudioBitDepthConvert/Program.cs
+++ AudioBitDepthConvert/Program.cs
@@ -1,9 +1,38 @@
 ﻿namespace AudioBitDepthConvert;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Enter the path of the directory: ");
+        var directoryPath = Console.ReadLine();
+
+        if (Directory.Exists(directoryPath))
+        {
+            ConvertFiles(directoryPath);
+
+            Console.WriteLine("Converting completed successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Directory not found.");
+        }
+    }
+    
+    private static void ConvertFiles(string directoryPath) 
+    {
+        var audioFiles = Directory.GetFiles(directoryPath, "*.wav", SearchOption.AllDirectories);
+
+        foreach (var file in audioFiles)
+        {
+            ConvertBitDepth(file);
+        }
+    }
+
+    private static void ConvertBitDepth(string filePath)
+    {
+        Console.WriteLine(filePath);
+
+        // code
     }
 }
